556c8630c602f7cd576fb93fbf58acfd3ecf31b0
minhnhut2306 <125520970+Hi-hoc-code@users.noreply.github.com>
2024-12-04 21:16:16 +0700

    ,

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const routes = require('./src/routes/routes')
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');  
+const routes = require('./src/routes/routes');  
 
+const connectDB = require('./src/config/db');  
+dotenv.config(); 
 
-const connectDB = require('./src/config/db');
-dotenv.config();
 const app = express();
+app.use(cors());
 app.use(express.json());
-
 connectDB();
 app.use('/', routes);
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
