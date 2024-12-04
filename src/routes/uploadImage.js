ce2840c95ae00da5d170b568ca41b161fdc18bc4
minhnhut2306 <125520970+Hi-hoc-code@users.noreply.github.com>
2024-12-04 23:14:17 +0700

    fix

--- src/routes/uploadImage.js
+++ src/routes/uploadImage.js
@@ -1,22 +1,24 @@
 const express = require('express')
 const router = express.Router();
 const cloudinary = require('./../config/cloudinary');
-const upload = multer({ dest: 'uploads/' }); 
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 router.post('/upload', upload.single('image'), async (req, res) => {
     try {
-      if (!req.file) {
-        return res.status(400).json({ message: 'No file uploaded' });
-      }
-      const result = await cloudinary.uploader.upload(req.file.path);
-      res.json({
-        secure_url: result.secure_url,
-      });
+        if (!req.file) {
+            return res.status(400).json({ message: 'No file uploaded' });
+        }
+        const result = await cloudinary.uploader.upload(req.file.path);
+        res.json({
+            secure_url: result.secure_url,
+        });
     } catch (error) {
-      console.error('Error uploading image:', error);
-      res.status(500).json({ message: 'Error uploading image', error });
+        console.error('Error uploading image:', error);
+        res.status(500).json({ message: 'Error uploading image', error });
     }
-  });
+});
+
 
 module.exports = router
 
