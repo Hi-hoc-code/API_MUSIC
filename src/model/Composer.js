4d4fe01f3f6b49b933628ea15609888f528f010d
minhnhut2306 <125520970+Hi-hoc-code@users.noreply.github.com>
2024-12-05 15:46:43 +0700

    v=bio

--- src/model/Composer.js
+++ src/model/Composer.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const Composer = new Schema({
     nameComposer: { type: String, require: true },
-    bioCompoer: { type: String },
+    bioComposer: { type: String },
     imgComposer: { type: String, default: "https://res.cloudinary.com/dmkyhh1cd/image/upload/v1731951465/null_pztg8k.jpg" }
 })
 
