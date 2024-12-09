0055188827153fbd020c9cfc75ae80ad0f03cd4d
minhnhut2306 <125520970+Hi-hoc-code@users.noreply.github.com>
2024-12-09 10:40:14 +0700

    sửa name

--- src/middleware/checkExists/SongExists.js
+++ src/middleware/checkExists/SongExists.js
@@ -1,8 +1,8 @@
 const Song = require("../../model/Song");
 
 const check_song_exists = async (req, res, next) => {
-    const { name_song } = req.body;
-    const song_exists = await Song.findOne({ name_song });
+    const { nameSong } = req.body;
+    const song_exists = await Song.findOne({ nameSong });
     if (song_exists) {
         return res.status(400).json({ message: 'bài hát đã tồn tại' });
     }
